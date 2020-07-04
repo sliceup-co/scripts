477c551c845ea41ec2e9bd392f33f6d361ae33b9
Andres <andres@sliceup.co>
2020-07-04 21:52:10 +0000

    M

--- send_logs_parallel/README.md
+++ send_logs_parallel/README.md
@@ -4,11 +4,11 @@ To use this script, first build with:
 
 Then set the following environment variables:
 
-export LOGS_DIR="/home/aarpi/slice1/scripts/send_logs_parallel/mounted_logs" (absolute path to the dir where you have the logs to be sent)
+export LOGS_DIR="/home/csroot/drain/logs"
 export PROT=tcp
 export DST=10.12.2.150:5140
-export TAG=TAG1
+export TAG=tag
 export MESSAGES_PER_SECOND=1000
 
 
-docker run -v $LOGS_DIR:/app/mounted_logs --log-driver syslog --log-opt syslog-address=$PROT://$DST --log-opt syslog-format=rfc5424  --log-opt tag=$TAG -e messages_per_second=$MESSAGES_PER_SECOND log-sender
+sudo docker run -v $LOGS_DIR:/app/mounted_logs --log-driver syslog --log-opt syslog-address=$PROT://$DST --log-opt syslog-format=rfc5424  --log-opt tag=$TAG -e messages_per_second=$MESSAGES_PER_SECOND -d log-sender
--- send_logs_parallel/main.py
+++ send_logs_parallel/main.py
@@ -16,30 +16,33 @@ if __name__ == "__main__":
     messages_per_second = float(sys.argv[1])
     rate = (1 / messages_per_second) / 2.0
 
+    while (True):
+        onlyfiles = [f for f in listdir(logs_dir) if isfile(join(logs_dir, f))]
+        onlyfiles = [f for f in onlyfiles if ((len(f) > 4) and (f[-4:] == ".log"))]
+        if (len(onlyfiles) > 100):
+            onlyfiles = onlyfiles[:100]
+        
+        file_handlers = [open(join(logs_dir,filename), "r") for filename in onlyfiles]
+        count = [0] * len(file_handlers)
 
-    onlyfiles = [f for f in listdir(logs_dir) if isfile(join(logs_dir, f))]
-    onlyfiles = [f for f in onlyfiles if ((len(f) > 4) and (f[-4:] == ".log"))]
+        global_counter = 0
+        while(len(file_handlers) > 0):
+            global_counter += 1
+            if (global_counter % 10000 == 0):
+                print(global_counter)
+
+            idx = random.randint(0, len(file_handlers) - 1)
+            try:
+                a = next(file_handlers[idx])
+                print(a[:-1] + "(" + str(global_counter) + ")")
+                count[idx] += 1
+                time.sleep(rate)
+            except KeyboardInterrupt:
+                sys.exit()
     
-    file_handlers = [open(join(logs_dir,filename), "r") for filename in onlyfiles]
-    count = [0] * len(file_handlers)
-
-    global_counter = 0
-    while(len(file_handlers) > 0):
-        global_counter += 1
-        if (global_counter % 10000 == 0):
-            print(global_counter)
-
-        idx = random.randint(0, len(file_handlers) - 1)
-        try:
-            a = next(file_handlers[idx])
-            print(a[:-1] + "(" + str(global_counter) + ")")
-            count[idx] += 1
-            time.sleep(rate)
-        except KeyboardInterrupt:
-            sys.exit()
- 
-        except:
-            print("Removing {} ({})".format(onlyfiles[idx], count[idx]))
-            file_handlers.pop(idx)
-            count.pop(idx)
+            except:
+                print("Removing {} ({})".format(onlyfiles[idx], count[idx]))
+                file_handlers.pop(idx)
+                count.pop(idx)
+        print("Starting another round ...")
 
