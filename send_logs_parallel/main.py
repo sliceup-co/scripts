fd4c8757bbd6b8fdfaf8cdff95d10bb10b256aab
Andres <andres@sliceup.co>
2020-07-27 19:23:42 -0300

    M

--- send_logs_parallel/main.py
+++ send_logs_parallel/main.py
@@ -9,40 +9,53 @@ import subprocess
 
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 2):
+    if (len(sys.argv) != 2) and (len(sys.argv) != 3):
         raise Exception("Please provide a directory for the log files and a target file")
     
     logs_dir = "mounted_logs"
     messages_per_second = float(sys.argv[1])
     rate = (1 / messages_per_second) / 2.0
-
-    while (True):
-        onlyfiles = [f for f in listdir(logs_dir) if isfile(join(logs_dir, f))]
-        onlyfiles = [f for f in onlyfiles if ((len(f) > 4) and (f[-4:] == ".log"))]
-        if (len(onlyfiles) > 100):
-            onlyfiles = onlyfiles[:100]
-        
-        file_handlers = [open(join(logs_dir,filename), "r") for filename in onlyfiles]
-        count = [0] * len(file_handlers)
-
-        global_counter = 0
-        while(len(file_handlers) > 0):
-            global_counter += 1
-            if (global_counter % 10000 == 0):
-                print(global_counter)
-
-            idx = random.randint(0, len(file_handlers) - 1)
-            try:
-                a = next(file_handlers[idx])
-                print(a[:-1] + "(" + str(global_counter) + ")")
-                count[idx] += 1
-                time.sleep(rate)
-            except KeyboardInterrupt:
-                sys.exit()
+    if (len(sys.argv) == 3) and (sys.argv[2].lower() == "true"):
+        loop_forever = True
+    else:
+        loop_forever = False
     
-            except:
-                print("Removing {} ({})".format(onlyfiles[idx], count[idx]))
-                file_handlers.pop(idx)
-                count.pop(idx)
-        print("Starting another round ...")
+    while(True):
+        onlyfiles_reserve = [f for f in listdir(logs_dir) if isfile(join(logs_dir, f))]
+        onlyfiles_reserve = [f for f in onlyfiles_reserve if ((len(f) > 4) and (f[-4:] == ".log"))]
+        while (len(onlyfiles_reserve) > 0):
+
+            if (len(onlyfiles_reserve) > 10):
+                onlyfiles = onlyfiles_reserve[:10]
+                onlyfiles_reserve = onlyfiles_reserve[10:]
+            else:
+                onlyfiles = onlyfiles_reserve[:]
+                onlyfiles_reserve = onlyfiles_reserve[len(onlyfiles_reserve):]
+            
+            file_handlers = [open(join(logs_dir,filename), "r") for filename in onlyfiles]
+            count = [0] * len(file_handlers)
+
+            global_counter = 0
+            while(len(file_handlers) > 0):
+                global_counter += 1
+                if (global_counter % 10000 == 0):
+                    print(global_counter)
+
+                idx = random.randint(0, len(file_handlers) - 1)
+                try:
+                    a = next(file_handlers[idx])
+                    print(a[:-1] + "(" + str(global_counter) + ")")
+                    count[idx] += 1
+                    time.sleep(rate)
+                except KeyboardInterrupt:
+                    sys.exit()
+        
+                except:
+                    print("Removing {} ({})".format(onlyfiles[idx], count[idx]))
+                    file_handlers.pop(idx)
+                    count.pop(idx)
+            print("Starting another round ...")
+        
+        if (not loop_forever):
+            break
 
